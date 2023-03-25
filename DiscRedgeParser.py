15b87c2c5db5fd899f9b7a26caa510657c3bdf29
KevinDsouza03 <dsouzakev467@gmail.com>
2023-03-24 23:32:39 -0400

    Took out unneeded code. still imports

--- DiscRedgeParser.py
+++ DiscRedgeParser.py
@@ -15,22 +15,9 @@ from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
 import time
-#MTAxMjc3MjM2Mzc3NzA5MzcxMg.G3Y6pR.Zfxjr91X4kAz7spTheY6nxLNYd4TKrGHQWeahA Token
+token = 'MTAxMjc3MjM2Mzc3NzA5MzcxMg.G3Y6pR.Zfxjr91X4kAz7spTheY6nxLNYd4TKrGHQWeahA'
 #https://discord.com/api/oauth2/authorize?client_id=921811125799125023&permissions=527744384064&scope=bot 
 #from discreactbot import *
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
-options = Options()
-#options.add_argument('--headless')
-path_to_extension = r'G:\5.1.1_0'
-options.add_argument('--disable-gpu')
-options.add_argument('--ignore-certificate-errors')
-options.add_argument('--ignore-ssl-errors')
-options.add_argument('--disable-blink-features=AutomationControlled')
-options.add_argument('load-extension=' + path_to_extension)
-
 
 service = Service(executable_path="chromedriver")
 driver = webdriver.Chrome(service=service, options=options)
@@ -47,5 +34,5 @@ async def Start(ctx):
     
 #will define commands here, but functions in other. 
 
-bot.run('MTAxMjc3MjM2Mzc3NzA5MzcxMg.G3Y6pR.Zfxjr91X4kAz7spTheY6nxLNYd4TKrGHQWeahA')
+bot.run(token)
 
