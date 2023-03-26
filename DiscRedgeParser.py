1cb4d360bb6320b76f1e1b8eea73efb951acf5ca
KevinDsouza03 <dsouzakev467@gmail.com>
2023-03-26 13:50:10 -0400

    time

--- DiscRedgeParser.py
+++ DiscRedgeParser.py
@@ -1,6 +1,7 @@
 #Improved Redge after more experience
 import functionheaders as fs
 from functionheaders import * 
+import time
 
 token = 'MTAxMjc3MjM2Mzc3NzA5MzcxMg.G3Y6pR.Zfxjr91X4kAz7spTheY6nxLNYd4TKrGHQWeahA'
 #https://discord.com/api/oauth2/authorize?client_id=921811125799125023&permissions=527744384064&scope=bot 
@@ -24,9 +25,9 @@ async def Start(ctx):
         SRObjects.append({"SR" : i.strip(),"title" : "FirstIteration", "link" : "FirstIteration"})
     #populate array with sr's
     for i in range(1,1000):
-        sleep(15)
+        time.sleep(15)
         for iter in SRObjects:
-            sleep(5)
+            time.sleep(5)
             result = fs.Reddit_parse(iter)
             if (result != -1):
                 embed=discord.Embed(title=iter["title"],description=iter["SR"])
